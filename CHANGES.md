1ce73e7e6a31882216ce55e28f70a7ef49619f8e
Tim McCormack <tmccormack@brightcove.com>
2013-04-30 17:23:20 -0400

    Release BC fork 1.0.4-bc0.

---
+++ CHANGES.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## v1.0.4-bc0
+
+* Compile test Java code after Java and Clojure source
+* Accept additional JVM options: `:lein-junit :jvm-opts`
--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
-(defproject lein-junit "1.0.4-BC-SNAPSHOT"
+(defproject org.clojars.brightcove/lein-junit "1.0.4-bc0"
   :author "Caspar Florian Ebeling, Roman Scherer"
   :description "JUnit plugin for Leiningen. Max Mao made a change to compile test code only."
-  :url "http://github.com/febeling/lein-junit"
+  :url "http://github.com/timmc/lein-junit"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :min-lein-version "2.0.0"
   :dependencies [[ant/ant-junit "1.6.5"]
                  [lancet "1.0.1"]
